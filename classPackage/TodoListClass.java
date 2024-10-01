fe38a29e39ab921d296ab371964ab086d4fb996d
spyrash <diego.petrucci@outlook.com>
2024-10-02 00:18:16 +0200

    feat: first step into scanner function and package heriarchy

--- classPackage/TodoListClass.java
+++ classPackage/TodoListClass.java
@@ -1,5 +1,5 @@
 package classPackage;
 
 public class TodoListClass {
-
+	
 }
--- servicePackage/TodoListService.java
+++ servicePackage/TodoListService.java
@@ -1,5 +1,11 @@
 package servicePackage;
-
+import java.util.Scanner;
 public class TodoListService {
-
+	public void takeAndPrint() {
+		Scanner scannerInput = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("add task number:");
+	    int taskNumber = scannerInput.nextInt();  // Read user input
+	    System.out.println("task number is: "+ taskNumber);  // Output user input
+	    scannerInput.close();
+	}
 }
