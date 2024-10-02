157b27e6b499376140fb60cef503e97ce51aa43c
spyrash <diego.petrucci@outlook.com>
2024-10-02 14:00:18 +0200

    feat: added startServiceSequence, going for a terminal msg approach

--- mainPackage/Main.java
+++ mainPackage/Main.java
@@ -1,12 +1,12 @@
 package mainPackage;
 
-import java.util.*;
 import servicePackage.*;
 
 
 public class Main {
 	public static void main(String[] args) {
 		TodoListService service = new TodoListService();
+		service.startServiceSequence();
 		service.takeAndPrint();
 		}
 	
--- servicePackage/TodoListService.java
+++ servicePackage/TodoListService.java
@@ -1,8 +1,32 @@
 package servicePackage;
 import java.util.Scanner;
+
 public class TodoListService {
+	Scanner scannerInput;
+	
+	public TodoListService() {
+		this.scannerInput = new Scanner(System.in);
+	}
+	public void startServiceSequence() {
+		System.out.println("Insert the number of task you want to add in the todolist:");
+		String outputMsg;
+		int numberOfTasks = scannerInput.nextInt();
+		if (numberOfTasks > 1) {
+			outputMsg = "Nice! let's start to configure each of the tasks:";
+		} else {
+			outputMsg = "Nice! let's start to configure this single task";
+		}
+		System.out.println(outputMsg);
+		for(int task_n = 1; task_n < numberOfTasks ; task_n++) {
+			addSingleTask();
+		}
+	}
+	
+	private void addSingleTask() {
+		
+	}
+	
 	public void takeAndPrint() {
-		Scanner scannerInput = new Scanner(System.in);  // Create a Scanner object
 	    System.out.println("add task number:");
 	    int taskNumber = scannerInput.nextInt();  // Read user input
 	    System.out.println("task number is: "+ taskNumber);  // Output user input
